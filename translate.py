077621e09bf478d7cf7fa8aa5eaa5564b6da79a1
usrm7 <>
2021-07-07 00:42:06 -0600

    updated formatting per black style

--- translate.py
+++ translate.py
@@ -98,16 +98,16 @@ def make_translation_request():
     )
 
     params = (
-                 ("client", "at"),
-                 ("dt", ["t", "ld", "qca", "rm", "bd"]),
-                 ("dj", "1"),
-                 ("hl", "%s"),
-                 ("ie", "UTF-8"),
-                 ("oe", "UTF-8"),
-                 ("inputm", "2"),
-                 ("otf", "2"),
-                 ("iid", "1dd3b944-fa62-4b55-b330-74909a99969e"),
-             ) + query_tuple
+        ("client", "at"),
+        ("dt", ["t", "ld", "qca", "rm", "bd"]),
+        ("dj", "1"),
+        ("hl", "%s"),
+        ("ie", "UTF-8"),
+        ("oe", "UTF-8"),
+        ("inputm", "2"),
+        ("otf", "2"),
+        ("iid", "1dd3b944-fa62-4b55-b330-74909a99969e"),
+    ) + query_tuple
 
     response = requests.post(
         "https://translate.google.com/translate_a/single",
